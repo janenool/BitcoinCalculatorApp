234ac79284ef1ebb4a22cc52f82503d66e9767bd
janenool <janenool@gmail.com>
2022-03-17 20:11:39 +0200

    BitcoinApp

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitCoinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetRates_Click(object sender, EventArgs e)
+        {
+            if (currencyCombo.SelectedItem.ToString() == "EUR")
+            {
+                resultLabel.Visible = true;
+                resulttextBox.Visible = true;
+                BitCoinRates bitcoin = GetRates();
+                float result = Int32.Parse(amountOfCoinBox.Text) * bitcoin.bpi.EUR.rate_float;
+                resulttextBox.Text = $"{result.ToString()} {bitcoin.bpi.EUR.code}";
+            }
+            else
+            {
+                resultLabel.Visible = true;
+                resulttextBox.Visible = true;
+                BitCoinRates bitcoin = GetRates();
+                float result = Int32.Parse(amountOfCoinBox.Text) * bitcoin.bpi.USD.rate_float;
+                resulttextBox.Text = $"{result.ToString()} {bitcoin.bpi.USD.code}";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static BitCoinRates GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitCoinRates bitcoin;
+            using(var resonseReader = new StreamReader(webStream))
+            {
+                var response = resonseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<BitCoinRates>(response);
+            }
+            return bitcoin;
+        }
+
+
+    }
+}
